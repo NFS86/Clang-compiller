bec541ac1224dae022732bc8d7ee2360dace58ec
NFS86 <jarbull86@gmail.com>
2022-03-10 13:05:41 +0000

    remove check
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- ci.sh
+++ ci.sh
@@ -66,15 +66,12 @@ function do_llvm() {
     curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d "disable_web_page_preview=true" -d "parse_mode=html" -d chat_id=$TG_CHAT_ID -d text="<b>$LLVM_NAME: Toolchain Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
     msg "$LLVM_NAME: Building llvm..."
     ./build-llvm.py \
-        --assertions \
         --clang-vendor "$LLVM_NAME" \
         --branch "main" \
-        --incremental \
-        --build-stage1-only \
-        --install-stage1-only \
         --projects "clang;lld;llvm;polly" \
-        --shallow-clone \
         --targets "ARM;AArch64" \
+        --shallow-clone \
+        --incremental \
         --defines "LLVM_PARALLEL_COMPILE_JOBS=8 LLVM_PARALLEL_LINK_JOBS=8"
         --build-type "Release" 2>&1 | tee build.log
     
