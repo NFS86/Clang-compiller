979e1a844d7011117b85880ae0af59f712ca3cf3
NFS86 <jarbull86@gmail.com>
2022-03-11 03:06:16 +0000

    bodo amat

--- build_llvm.sh
+++ build_llvm.sh
@@ -13,15 +13,6 @@ rel_date="$(date "+%Y%m%d")" # ISO 8601 format
 rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
 builder_commit="$(git rev-parse HEAD)"
 
-export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
-tg_post_build() {
-	curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
-	-F chat_id="$TG_CHAT_ID"  \
-	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
-	-F caption="$3"
-}
-
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d "disable_web_page_preview=true" -d "parse_mode=html" -d chat_id=$TG_CHAT_ID -d text="<b>$LLVM_NAME: Toolchain Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
 msg "$LLVM_NAME: Building llvm..."
 ./build-llvm.py \
@@ -33,10 +24,3 @@ msg "$LLVM_NAME: Building llvm..."
     --incremental \
     --defines "LLVM_PARALLEL_COMPILE_JOBS=8 LLVM_PARALLEL_LINK_JOBS=8" \
     --build-type "Release" 2>&1 | tee build.log
-
-# Check if the final clang binary exists or not.
-[ ! -f install/bin/clang-1* ] && {
-	err "Building LLVM failed ! Kindly check errors !!"
-	tg_post_build "build.log" "$TG_CHAT_ID" "Error Log"
-	exit 1
-}
